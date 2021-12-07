d0189e4d03dd671eef971f9559ebbd3db01591e5
yuhao <yuhaodai20030404@gmail.com>
2021-12-07 11:19:47 +0800

    12071119

---
+++ src/main/StarPyramid.java
@@ -0,0 +1,17 @@
+package main;
+
+public class StarPyramid {
+    public static void main(String[] args) {
+        for (int i =0;i<10;i++){
+            for (int j=0;j<19;j++){
+                if (10 - i >= j||10+i<=j){
+                    System.out.print(" ");
+
+                }else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
